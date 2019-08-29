aa78e9d5139e1c40e594c5c12278540a0a0f6c77
iKurisu <chris.epic01@gmail.com>
2019-08-29 08:30:48 +0200

    Update regex to match negative numbers

--- src/utils/transition.ts
+++ src/utils/transition.ts
@@ -12,7 +12,7 @@ export const getPropFunction = (prop: string | number): string => {
 };
 
 export const getValue = (prop: string | number): number =>
-  typeof prop === "string" ? +prop.match(/\d+/g)[0] : prop;
+  typeof prop === "string" ? +prop.match(/-?\d+/g)[0] : prop;
 
 export const getUnit = (prop: string | number): string =>
   typeof prop === "string" ? prop.match(/px|%|vw|vh/g)[0] : null;
