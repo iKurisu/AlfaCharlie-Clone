a973819c32cef12c9e82f4d06651f29e6ad30bd6
iKurisu <chris.epic01@gmail.com>
2019-09-02 17:05:07 +0200

    Add logic to handle transform properties with multiple values

--- src/hooks/transition/stringify.ts
+++ src/hooks/transition/stringify.ts
@@ -1,4 +1,4 @@
-import { Properties, MappedProperties } from "./types";
+import { Properties, MappedProperties, MappedProperty } from "./types";
 import { getCurrentValue } from "utils/transition";
 
 const stringify = (fn: string, value: number, unit: string): string | number =>
@@ -7,31 +7,66 @@ const stringify = (fn: string, value: number, unit: string): string | number =>
 const at = (moment: "end" | "start", ease: number): boolean =>
   ease === (moment === "end" ? 1 : 0);
 
+const getStringifiedValueAtTime = (
+  ease: number,
+  { function: fn, initialValue, targetValue, unit }: MappedProperty
+): string | number => {
+  if (initialValue === null && !at("end", ease)) {
+    return null;
+  }
+
+  const value =
+    initialValue === null && at("end", ease)
+      ? targetValue
+      : targetValue === null
+      ? initialValue
+      : getCurrentValue({ initialValue, targetValue }, ease);
+
+  return stringify(fn, value, unit);
+};
+
+const stringifyTransformProperties = (
+  mappedProperty: MappedProperty[],
+  ease: number
+): string =>
+  mappedProperty
+    .reduce(
+      (stringifiedProperties: string[], property: MappedProperty): string[] => {
+        const value = getStringifiedValueAtTime(ease, property) as string;
+
+        return value !== null
+          ? [...stringifiedProperties, value]
+          : stringifiedProperties;
+      },
+      []
+    )
+    .join(" ");
+
 const stringifyProperties = (
   mappedProperties: MappedProperties,
   ease: number
 ): Properties =>
   Object.keys(mappedProperties).reduce(
-    (properties: Properties, property: keyof Properties): Properties => {
-      const {
-        function: fn,
-        initialValue,
-        targetValue,
-        unit
-      } = mappedProperties[property];
-
-      if (initialValue === null && !at("end", ease)) {
-        return properties;
+    (properties: Properties, key: keyof Properties): Properties => {
+      if (key === "transform" && Array.isArray(mappedProperties[key])) {
+        return {
+          ...properties,
+          ...{
+            transform: stringifyTransformProperties(
+              mappedProperties[key] as MappedProperty[],
+              ease
+            )
+          }
+        };
       }
 
-      const value =
-        initialValue === null && at("end", ease)
-          ? targetValue
-          : targetValue === null
-          ? initialValue
-          : getCurrentValue({ initialValue, targetValue }, ease);
+      const value = getStringifiedValueAtTime(ease, mappedProperties[
+        key
+      ] as MappedProperty);
 
-      return { ...properties, ...{ [property]: stringify(fn, value, unit) } };
+      return value !== null
+        ? { ...properties, ...{ [key]: value } }
+        : properties;
     },
     {}
   );
