8511670b15edf3509450a0f5b9eae5a44f0c0bf0
iKurisu <chris.epic01@gmail.com>
2019-09-04 08:44:55 +0200

    Prettified

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import App from './App';
-import renderer from 'react-test-renderer';
+import React from "react";
+import App from "./App";
+import renderer from "react-test-renderer";
 
-it('renders correctly', () => {
-  const component: renderer.ReactTestRenderer = renderer.create(<App />)
+it("renders correctly", (): void => {
+  const component: renderer.ReactTestRenderer = renderer.create(<App />);
   expect(component).toMatchSnapshot();
-})
+});
