9c67627d361096e96eadfc2c07346e030ed2f66d
iKurisu <chris.epic01@gmail.com>
2019-09-13 08:39:59 +0200

    Update props

--- src/components/menu/navigation/link/Character.test.tsx
+++ src/components/menu/navigation/link/Character.test.tsx
@@ -3,6 +3,8 @@ import renderer from "react-test-renderer";
 import Character from "./Character";
 
 it("renders correctly", (): void => {
-  const char = renderer.create(<Character char={"d"} order={1} />);
+  const char = renderer.create(
+    <Character char={"d"} order={1} isOpen={true} />
+  );
   expect(char).toMatchSnapshot();
 });
