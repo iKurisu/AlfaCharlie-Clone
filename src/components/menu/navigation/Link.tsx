fed59ab331884a9f60a5f205131aee754b81f0cc
iKurisu <chris.epic01@gmail.com>
2019-09-13 08:40:14 +0200

    Refactor

--- src/components/menu/navigation/Link.tsx
+++ src/components/menu/navigation/Link.tsx
@@ -14,23 +14,21 @@ const Link = ({
   fadeInOrder,
   isOpen,
   updateHoveringElementId
-}: Props): JSX.Element => {
-  return (
-    <a className="menu-nav-link" onMouseEnter={updateHoveringElementId}>
-      <span className="link-characters">
-        {link.split("").map(
-          (character: string, id: number): JSX.Element => (
-            <Character
-              char={character}
-              order={fadeInOrder[id]}
-              isOpen={isOpen}
-              key={id}
-            />
-          )
-        )}
-      </span>
-    </a>
-  );
-};
+}: Props): JSX.Element => (
+  <a className="menu-nav-link" onMouseEnter={updateHoveringElementId}>
+    <span className="link-characters">
+      {link.split("").map(
+        (character: string, id: number): JSX.Element => (
+          <Character
+            char={character}
+            order={fadeInOrder[id]}
+            isOpen={isOpen}
+            key={id}
+          />
+        )
+      )}
+    </span>
+  </a>
+);
 
 export default Link;
