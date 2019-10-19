c4f633125401eec79cdd49395de69cc6885c1210
iKurisu <chris.epic01@gmail.com>
2019-10-19 10:51:50 +0200

    Add slider animation

--- src/components/menu/Slider.tsx
+++ src/components/menu/Slider.tsx
@@ -1,4 +1,6 @@
 import React, { useRef } from "react";
+import useTransition from "hooks/useTransition";
+import useDidUpdateEffect from "hooks/useDidUpdateEffect";
 import "./Slider.scss";
 
 const urlSite = "https://alfacharlie.b-cdn.net/wp-content/uploads/";
@@ -11,11 +13,13 @@ const imageUrls = [
 ];
 
 interface Props {
+  isOpen: boolean;
   hoveringElementId: number;
   previousElementId: number;
 }
 
 const Slider = ({
+  isOpen,
   hoveringElementId,
   previousElementId
 }: Props): JSX.Element => {
@@ -23,6 +27,32 @@ const Slider = ({
     Math.abs(previousElementId - hoveringElementId) === 1 ? "1s" : "2s";
 
   const mask = useRef(null);
+
+  const revealSlider = useTransition(mask, {
+    from: { transform: "translateX(0)" },
+    to: { transform: "translateX(100%)" },
+    config: {
+      duration: 850,
+      delay: 800,
+      timing: [0.17, 0.5, 0.48, 1]
+    }
+  });
+
+  const hideSlider = useTransition(mask, {
+    from: { transform: "translateX(-100%)" },
+    to: { transform: "translateX(0)" },
+    config: {
+      duration: 100
+    }
+  });
+
+  useDidUpdateEffect((): void => {
+    if (isOpen) {
+      revealSlider();
+    } else {
+      hideSlider();
+    }
+  });
 
   return (
     <div className="menu-images -flex">
