7b1affb245cc77d5a2d834292cb59fe08707a4f9
iKurisu <chris.epic01@gmail.com>
2019-10-28 09:41:25 +0100

    Add unit regex

--- src/hooks/mediaQuery/regex.ts
+++ src/hooks/mediaQuery/regex.ts
@@ -2,3 +2,4 @@ export const type = /(?:min|max)(?=Width)/;
 export const breakpoint = /[(](?:min|max)Width: (\d+)px[)](?= (?:and|=>))/;
 export const orientation = /[(]orientation: (landscape)[)]/;
 export const value = /=> (\d+\.?\d+?)(?:px|vw)/;
+export const unit = /=> \d+\.?\d+?(px|vw)/;
