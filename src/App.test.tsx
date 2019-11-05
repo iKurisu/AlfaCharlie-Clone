8224a77ff6270cb785d3662962fe323a66ddb807
iKurisu <chris.epic01@gmail.com>
2019-11-05 09:38:24 +0100

    Remove test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import App from "./App";
-import renderer from "react-test-renderer";
-
-it("renders correctly", (): void => {
-  const component: renderer.ReactTestRenderer = renderer.create(<App />);
-  expect(component).toMatchSnapshot();
-});
