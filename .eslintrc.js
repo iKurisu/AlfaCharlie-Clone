a1ce97ddc17b542ff0642d9bbe3a8c4d2fc76e36
iKurisu <chris.epic01@gmail.com>
2019-11-05 09:32:24 +0100

    Update eslint functions rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,13 @@ module.exports = {
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": ["warn", {
+      allowExpressions: true,
+      allowTypedFunctionExpressions: true,
+      allowHigherOrderFunctions: true
+    }]
+  },
   parserOptions: {
     ecmaVersion: 2019,
     sourceType: 'module',
