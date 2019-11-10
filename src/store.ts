aa96da9a841fc7c08ee70bd482ff525bfc2786a0
iKurisu <chris.epic01@gmail.com>
2019-11-10 17:53:33 +0100

    Add redux thunk

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,15 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk, { ThunkMiddleware } from "redux-thunk";
 import menu from "modules/menu";
 
 const rootReducer = combineReducers({
   menu
 });
-const store = createStore(rootReducer);
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk as ThunkMiddleware)
+);
 
 export type AppState = ReturnType<typeof rootReducer>;
 
