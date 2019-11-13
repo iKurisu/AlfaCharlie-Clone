433d9b92eca7f3dc897800f18ac498e51a175538
iKurisu <chris.epic01@gmail.com>
2019-11-13 19:17:50 +0100

    Remove thunk

--- src/modules/menu/actions.ts
+++ src/modules/menu/actions.ts
@@ -1,7 +1,4 @@
-import { AppState } from "store";
 import { Types, MenuActionTypes } from "./types";
-import { getDuration } from "utils/slider";
-import { ThunkAction } from "redux-thunk";
 
 const toggleMenu = (): MenuActionTypes => ({
   type: Types.TOGGLE_MENU
@@ -16,28 +13,8 @@ const updatePreviousElement = (): MenuActionTypes => ({
   type: Types.UPDATE_PREVIOUS_ELEMENT
 });
 
-const hoverElement = (
-  elementID: number,
-  maxTransitionDuration: number
-): ThunkAction<void, AppState, null, MenuActionTypes> => (
-  dispatch,
-  getState
-) => {
-  dispatch(setHoveringElement(elementID));
-
-  const { menu } = getState();
-  const { hoveringElementID, previousElementID } = menu;
-  const delay = getDuration(
-    { current: hoveringElementID, previous: previousElementID },
-    maxTransitionDuration
-  );
-
-  setTimeout(() => dispatch(updatePreviousElement()), delay);
-};
-
 export default {
   toggleMenu,
   setHoveringElement,
-  updatePreviousElement,
-  hoverElement
+  updatePreviousElement
 };
