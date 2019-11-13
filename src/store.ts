6117c30940fdf4fdb3b89bf795b22bdedf78eb47
iKurisu <chris.epic01@gmail.com>
2019-11-13 19:14:40 +0100

    Add hero module

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk, { ThunkMiddleware } from "redux-thunk";
 import menu from "modules/menu";
+import hero from "modules/hero";
 
 const rootReducer = combineReducers({
-  menu
+  menu,
+  hero
 });
 
 const store = createStore(
