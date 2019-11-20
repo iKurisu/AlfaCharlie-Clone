27eff0f1447937e69de73a9ca573d253b9fc68b7
iKurisu <chris.epic01@gmail.com>
2019-11-20 08:49:51 +0100

    Add arrows

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -127,8 +127,8 @@ const Home = ({
                   viewBox="0 0 157.34 51.71"
                   xmlSpace="preserve"
                 >
-                  <polyline points="128.65,45.62 148.36,25.91 128.47,6.02 "></polyline>
-                  <line x1="8.46" y1="25.91" x2="147.73" y2="25.91"></line>
+                  <polyline points="128.65,45.62 148.36,25.91 128.47,6.02 " />
+                  <line x1="8.46" y1="25.91" x2="147.73" y2="25.91" />
                 </svg>
                 <span className="link-text-wrapper">
                   <span className="link-text">View our work</span>
@@ -180,6 +180,36 @@ const Home = ({
                     key={id}
                   ></span>
                 ))}
+              </div>
+            </div>
+            <div className="hero-arrows">
+              <div className="arrow-prev">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  version="1.1"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 157.34 51.71"
+                  xmlSpace="preserve"
+                >
+                  <polyline points="128.65,45.62 148.36,25.91 128.47,6.02 " />
+                  <line x1="8.46" y1="25.91" x2="147.73" y2="25.91" />
+                </svg>
+              </div>
+              <div className="arrow-next">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  version="1.1"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 157.34 51.71"
+                  xmlSpace="preserve"
+                >
+                  <polyline points="128.65,45.62 148.36,25.91 128.47,6.02 " />
+                  <line x1="8.46" y1="25.91" x2="147.73" y2="25.91" />
+                </svg>
               </div>
             </div>
             <div
