2233548925d4df3c131cb881a6a18de8596e3030
iKurisu <chris.epic01@gmail.com>
2019-11-27 15:05:58 +0100

    Fix test

--- src/pages/home/__tests__/Hero.test.tsx
+++ src/pages/home/__tests__/Hero.test.tsx
@@ -4,7 +4,6 @@ import { Hero, Props } from "../Hero";
 
 describe("hero", (): void => {
   let hero: ShallowWrapper;
-  let dots: ShallowWrapper;
 
   const toggleIntro = jest.fn();
   const swipeSlide = jest.fn();
@@ -16,57 +15,12 @@ describe("hero", (): void => {
     swipeSlide: (id: number, delay: number) => swipeSlide.bind(null, id, delay)
   };
 
-  const updateWrappers = (newProps?: {
-    currentSlideID?: number;
-    previousSlideID?: number;
-  }): void => {
-    hero = shallow(<Hero {...{ ...props, ...newProps }} />);
-    dots = hero.find(".hero-slider-dots");
-  };
-
   beforeEach((): void => {
     hero = shallow(<Hero {...props} />);
-    dots = hero.find(".hero-slider-dots");
   });
 
   afterEach((): void => {
     jest.clearAllMocks();
-  });
-
-  it("has correct active dot", (): void => {
-    expect(dots.childAt(0).hasClass("active")).toBe(true);
-    expect(dots.childAt(4).hasClass("active")).toBe(false);
-
-    hero.setProps({ currentSlideID: 4 });
-    updateWrappers({ currentSlideID: 4 });
-
-    expect(dots.childAt(4).hasClass("active")).toBe(true);
-    expect(dots.childAt(0).hasClass("active")).toBe(false);
-  });
-
-  it("has correct progress indicator", (): void => {
-    let progress = hero.find(".hero-slider-current");
-
-    expect(progress.childAt(0).props().style.transform).toBe("translateY(0%)");
-
-    dots.childAt(3).simulate("mouseover");
-    progress = hero.find(".hero-slider-current");
-
-    expect(progress.childAt(0).props().style.transform).toBe(
-      "translateY(-300%)"
-    );
-
-    dots.childAt(3).simulate("mouseleave");
-    progress = hero.find(".hero-slider-current");
-
-    expect(progress.childAt(0).props().style.transform).toBe("translateY(0%)");
-  });
-
-  it("fires click event on dot", (): void => {
-    dots.childAt(2).simulate("click");
-
-    expect(swipeSlide.mock.calls.length).toBe(1);
-    expect(swipeSlide.mock.calls[0]).toEqual([2, 2000]);
   });
 
   it("disables arrows correctly", (): void => {
