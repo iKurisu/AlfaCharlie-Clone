4e7b7ce255b167992668154c6490649b4c1e1cf6
iKurisu <chris.epic01@gmail.com>
2019-12-02 09:05:18 +0100

    Add agency

---
+++ src/pages/Agency.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const Agency = (): JSX.Element => {
+  useEffect((): void => {
+    document.title =
+      "Branding, Web Design and Graphic Design | Alpha Charlie | What We Do";
+  });
+
+  return (
+    <React.Fragment>
+      <div className="row"></div>
+    </React.Fragment>
+  );
+};
+
+export default Agency;
