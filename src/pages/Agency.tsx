5545a0aed181896cbe0af0d1a00200cca68f27c9
iKurisu <chris.epic01@gmail.com>
2019-12-04 10:03:25 +0100

    add info component

--- src/pages/Agency.tsx
+++ src/pages/Agency.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Brief from "./agency/Brief";
+import Info from "./shared/Info";
 
 const Agency = (): JSX.Element => {
   useEffect((): void => {
@@ -11,6 +12,25 @@ const Agency = (): JSX.Element => {
     <React.Fragment>
       <div className="row">
         <Brief />
+      </div>
+      <div className="separator" />
+      <div className="row">
+        <Info
+          header="what we do"
+          image={
+            "https://alfacharlie.b-cdn.net/wp-content/uploads/" +
+            "2019/06/alfa-charlie-agency.jpg"
+          }
+          title="Strategy-led design with a fresh approach."
+          text={
+            "We believe design should be meaningful. We aim to " +
+            "quiet the noise through a classic, clean aesthetic that " +
+            "allows your brand to emerge defined, not decorated — " +
+            "strengthening the relationship between client and customer."
+          }
+          link={false}
+          align={"left"}
+        />
       </div>
     </React.Fragment>
   );
