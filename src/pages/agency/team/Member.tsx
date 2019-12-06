671e43e7251df7543e86a06bfc00fe1958da07b1
iKurisu <chris.epic01@gmail.com>
2019-12-06 16:18:06 +0100

    add icon animation

--- src/pages/agency/team/Member.tsx
+++ src/pages/agency/team/Member.tsx
@@ -10,31 +10,30 @@ interface Props {
 }
 
 const Member = ({ img, name, position, about }: Props): JSX.Element => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleAbout = (): void => setIsActive(!isActive);
+
   return (
-    <div className="member">
+    <div
+      className={classList({
+        member: true,
+        active: isActive
+      })}
+    >
       <div className="member-image-wrapper">
         <div className="member-image">
           <img src={img} />
         </div>
       </div>
       <div className="member-info">
-        <h4 className="member-name">{name}</h4>
+        <h4 className="member-name" onClick={toggleAbout}>
+          {name}
+        </h4>
         <h6 className="member-position">{position}</h6>
         <span className="member-icon">
-          <span
-            className={classList({
-              "member-open": true,
-            })}
-          >
-            +
-          </span>
-          <span
-            className={classList({
-              "member-close": true,
-            })}
-          >
-            -
-          </span>
+          <span className="member-open">+</span>
+          <span className="member-close">-</span>
         </span>
         <p className="member-about">{about}</p>
       </div>
