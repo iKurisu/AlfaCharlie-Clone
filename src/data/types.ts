729205aec52b85e55444571c5502287da361acf6
iKurisu <chris.epic01@gmail.com>
2019-12-17 18:56:31 +0100

    add data

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,8 +1,28 @@
 export interface ACProject {
+  title: string;
+  description: string;
+  info: string;
   article: Article;
+  client: string;
+  year: number;
+  services: string[];
+  hero: string;
+  leading: (string | string[])[];
+  quote: Quote;
+  lazy: (string | string[])[]
 }
 
 interface Article {
   title: string[];
   img: string;
+}
+
+interface Quote {
+  text: string;
+  author: QuoteAuthor;
+}
+
+interface QuoteAuthor {
+  name: string;
+  position: string;
 }
