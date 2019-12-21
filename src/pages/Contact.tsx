da30b3e6eac8da696e53108e59bc0bc010a81120
iKurisu <chris.epic01@gmail.com>
2019-12-21 10:03:52 +0100

    add form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,47 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Arrow from "./contact/Arrow";
+import Form from "./contact/Form";
 import "./Contact.scss";
 
-const Contact = (): JSX.Element => (
-  <div className="contact">
-    <div className="contact-image">
-      <img
-        src={
-          "https://alfacharlie.b-cdn.net/wp-content/uploads/2019/" +
-          "05/Alfa-Charlie-Design-Agency-Contact-4.jpg"
-        }
+enum Forms {
+  START_A_PROJECT = "START_A_PROJECT",
+  SAY_HELLO = "SAY_HELLO"
+}
+
+type FormTypes = keyof typeof Forms;
+
+const Contact = (): JSX.Element => {
+  const [activeForm, setActiveForm] = useState<FormTypes>(null);
+
+  const startAProject = (): void => setActiveForm(Forms.START_A_PROJECT);
+  const sayHello = (): void => setActiveForm(Forms.SAY_HELLO);
+
+  return (
+    <div className="contact">
+      <div className="contact-image">
+        <img
+          src={
+            "https://alfacharlie.b-cdn.net/wp-content/uploads/2019/" +
+            "05/Alfa-Charlie-Design-Agency-Contact-4.jpg"
+          }
+        />
+      </div>
+      <div className="contact-labels">
+        <div className="contact-label-left" onClick={startAProject}>
+          <div className="contact-label">
+            <h2 className="label-title">
+              <span className="label-arrow">
+                <Arrow />
+              </span>
+              <span>start a project</span>
+            </h2>
+          </div>
+        </div>
+        <div className="contact-label-separator" />
+        <div className="contact-label-right" onClick={sayHello}>
+          <div className="contact-label">
+            <h2 className="label-title">
+              <span>say hello</span>
+              <span className="label-arrow">
+                <Arrow />
+              </span>
+            </h2>
+          </div>
+        </div>
+      </div>
+      <Form
+        names={["name", "email", "company", "project", "birthday"]}
+        show={activeForm === Forms.START_A_PROJECT}
+      />
+      <Form
+        names={["name", "email", "hello", "birthday"]}
+        show={activeForm === Forms.SAY_HELLO}
       />
     </div>
-    <div className="contact-labels">
-      <div className="contact-label-left">
-        <div className="contact-label">
-          <h2 className="label-title">
-            <span className="label-arrow">
-              <Arrow />
-            </span>
-            <span>start a project</span>
-          </h2>
-        </div>
-      </div>
-      <div className="contact-label-separator" />
-      <div className="contact-label-right">
-        <div className="contact-label">
-          <h2 className="label-title">
-            <span>say hello</span>
-            <span className="label-arrow">
-              <Arrow />
-            </span>
-          </h2>
-        </div>
-      </div>
-      <div className="contact-form-wrapper">
-        <div className="contact-form"></div>
-      </div>
-      <div className="contact-form-wrapper">
-        <div className="contact-form"></div>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Contact;
---
+++ src/pages/contact/Form.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { classList } from "utils/class";
+import "./Form.scss";
+
+interface Props {
+  names: string[];
+  show: boolean;
+}
+
+interface Lookup {
+  [k: string]: string;
+}
+
+const labels: Lookup = {
+  name: "Ahoy. What's your name?",
+  email: "What's your email?",
+  company: "Tell us a bit about your company.",
+  project: "Tell us a bit about your project.",
+  hello: "What would you like to chat about?",
+  birthday: "We love celebrations. When is your birthday?"
+};
+
+const errors: Lookup = {
+  name: "Please enter a valid name.",
+  email: "Please enter a valid email.",
+  company: "Please tell us about your company.",
+  project: "Please tell us about your project.",
+  hello: "Please enter a valid message.",
+  birthday: "Please provide a valid birthday."
+};
+
+const Form = ({ names, show }: Props): JSX.Element => {
+  const [slide, setSlide] = useState(0);
+
+  return (
+    <div className={classList(["contact-form-wrapper", { "-active": show }])}>
+      <form className="contact-form">
+        <div className="form-labels">
+          {names.map((name, id) => (
+            <label
+              className={classList(["form-label", { "-active": slide === id }])}
+              htmlFor={name}
+              key={id}
+            >
+              <h3>{labels[name]}</h3>
+            </label>
+          ))}
+        </div>
+        <div className="form-inputs">
+          {names.map((name, id) => (
+            <input
+              className={classList(["form-input", { "-active": slide === id }])}
+              name={name}
+              key={id}
+            />
+          ))}
+        </div>
+        <div className="form-errors">
+          {names.map((name, id) => (
+            <span
+              className={classList(["form-error", { "-active": slide === id }])}
+              key={id}
+            >
+              {errors[name]}
+            </span>
+          ))}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
