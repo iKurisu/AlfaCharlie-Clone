64e6bb49994328aeda5f5e6d7fd368e9fa21a999
iKurisu <chris.epic01@gmail.com>
2019-12-25 10:33:06 +0100

    remove snapshots

--- src/components/__tests__/Header.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Header from "../Header";
-
-it("renders correctly", (): void => {
-  const header = renderer.create(<Header />);
-
-  expect(header).toMatchSnapshot();
-});
