795ba88b46c1d1b07c94f9a85cb9fca92b3fde52
iKurisu <chris.epic01@gmail.com>
2019-12-25 10:33:39 +0100

    add tests

--- src/pages/contact/__tests__/Form.test.tsx
+++ src/pages/contact/__tests__/Form.test.tsx
@@ -6,6 +6,18 @@ describe("Form", (): void => {
   let wrapper: ShallowWrapper;
   const handleInput = jest.fn();
 
+  const is = (className: string) => (
+    selector: string,
+    index: number = 0
+  ): boolean =>
+    wrapper
+      .find(selector)
+      .at(index)
+      .hasClass(className);
+
+  const isActive = is("-active");
+  const isDisabled = is("-disabled");
+
   beforeEach((): void => {
     wrapper = shallow(
       <Form
@@ -18,15 +30,11 @@ describe("Form", (): void => {
   });
 
   it("has correct class", (): void => {
-    expect(wrapper.find(".contact-form-wrapper").hasClass("-active")).toBe(
-      false
-    );
+    expect(isActive(".contact-form-wrapper")).toBe(false);
 
     wrapper.setProps({ show: true });
 
-    expect(wrapper.find(".contact-form-wrapper").hasClass("-active")).toBe(
-      true
-    );
+    expect(isActive(".contact-form-wrapper")).toBe(true);
   });
 
   it("handles input", (): void => {
@@ -35,5 +43,50 @@ describe("Form", (): void => {
     input.simulate("change");
 
     expect(handleInput.mock.calls.length).toBe(1);
+  });
+
+  it("shows error when next arrow is clicked and input is invalid", (): void => {
+    const next = wrapper.find(".arrow-next");
+
+    expect(isActive(".form-error")).toBe(false);
+
+    next.simulate("click");
+
+    expect(isActive(".form-error")).toBe(true);
+  });
+
+  it("changes slides correctly", (): void => {
+    wrapper.setProps({ form: { a: "sldkf", b: "", c: "" } });
+
+    expect(isDisabled(".arrow-prev")).toBe(true);
+    expect(isActive(".form-label")).toBe(true);
+    expect(isActive(".form-label", 1)).toBe(false);
+
+    const next = wrapper.find(".arrow-next");
+
+    next.simulate("click");
+
+    expect(isDisabled(".arrow-prev")).toBe(false);
+    expect(isActive(".form-label")).toBe(false);
+    expect(isActive(".form-label", 1)).toBe(true);
+  });
+
+  it("error is hidden once the slide is changed", (): void => {
+    const next = wrapper.find(".arrow-next");
+    const prev = wrapper.find(".arrow-prev");
+
+    next.simulate("click");
+
+    wrapper.setProps({ form: { a: "dfslkfs", b: "", c: "" } });
+
+    expect(isActive(".form-error")).toBe(true);
+
+    next.simulate("click");
+
+    expect(isActive(".form-error", 1)).toBe(false);
+
+    prev.simulate("click");
+
+    expect(isActive(".form-error")).toBe(false);
   });
 });
