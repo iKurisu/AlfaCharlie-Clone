cd048701b3ebbb0795e292d5ace4d4ae476790d9
iKurisu <chris.epic01@gmail.com>
2019-12-26 10:34:47 +0100

    add links for eachroute

--- src/components/VerticalNav.tsx
+++ src/components/VerticalNav.tsx
@@ -1,50 +1,150 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
 import { Switch, Route } from "react-router-dom";
-import Link from "./vertical_nav/Link";
+import PolicyLink from "./vertical_nav/PolicyLink";
 import SocialLink from "./vertical_nav/SocialLink";
 import ClickLink from "./vertical_nav/ClickLink";
+import { workActions } from "modules/work";
+import { WorkActionTypes, Filters } from "modules/work/types";
 import "./VerticalNav.scss";
 
-interface NavLink {
-  link: string;
-  href?: string;
-  click?: <T>() => T;
-  active?: boolean;
+interface MappedActions {
+  setFilter: (filter: Filters) => WorkActionTypes;
 }
 
-interface NavProps {
-  links?: NavLink[];
+interface OwnProps {
+  menu?: boolean;
   show?: boolean;
   delay?: number;
 }
 
-const VerticalNav = ({ links, show, delay = 0 }: NavProps): JSX.Element => {
-  const renderLinks = (): JSX.Element[] =>
-    links.map(
-      (link, id): JSX.Element => (
-        <li key={id}>
-          <Link link={link} show={show} options={{ delay, order: id }} />
-        </li>
-      )
-    );
+type Props = MappedActions & OwnProps;
+
+enum AgencyLinks {
+  EXPERTISE = "agency/EXPERTISE",
+  TEAM = "agency/TEAM",
+  CLIENTS = "agency/CLIENTS"
+}
+
+enum WorkLinks {
+  ALL = "work/ALL",
+  BRANDING = "work/BRANDING",
+  DIGITAL = "work/DIGITAL"
+}
+
+const VerticalNav = ({
+  menu,
+  show,
+  delay = 0,
+  setFilter
+}: Props): JSX.Element => {
+  const [activeLink, setActiveLink] = useState<AgencyLinks | WorkLinks>(
+    WorkLinks.ALL
+  );
+
+  const MenuLinks = (
+    <React.Fragment>
+      <li key={0}>
+        <SocialLink show={show} options={{ order: 0, delay }} />
+      </li>
+      <li key={1}>
+        <PolicyLink show={show} options={{ order: 1, delay }} />
+      </li>
+    </React.Fragment>
+  );
 
   const homeLinks = (
     <li key={0}>
-      <SocialLink link="Intagram" show={show} />
+      <SocialLink show={show} />
     </li>
   );
+
+  function click<T>(cb: (arg: T) => void) {
+    return (link: AgencyLinks | WorkLinks, arg: T) => () => {
+      if (activeLink === link) return;
+
+      setActiveLink(link);
+      cb(arg);
+    };
+  }
+
+  const clickWithScroll = click((scroll: number) =>
+    window.scrollTo({ top: scroll })
+  );
+
+  const clickWithDispath = click((filter: Filters) => {
+    setFilter(filter);
+    window.scrollTo({ top: 0 });
+  });
 
   const agencyLinks = (
     <React.Fragment>
       <li key={0}>
         <ClickLink
           link="Expertise"
-          click={toggle => () => {
-            toggle();
-            window.scrollTo({ top: window.innerHeight });
-          }}
+          click={clickWithScroll(
+            AgencyLinks.EXPERTISE,
+            window.innerHeight + 100
+          )}
           show={show}
+          isActive={activeLink === AgencyLinks.EXPERTISE}
           options={{ order: 0 }}
+        />
+      </li>
+      <li key={1}>
+        <ClickLink
+          link="Team"
+          click={clickWithScroll(
+            AgencyLinks.TEAM,
+            2 * window.innerHeight + 100
+          )}
+          show={show}
+          isActive={activeLink === AgencyLinks.TEAM}
+          options={{ order: 1 }}
+        />
+      </li>
+      <li key={2}>
+        <ClickLink
+          link="Clients"
+          click={clickWithScroll(
+            AgencyLinks.CLIENTS,
+            3 * window.innerHeight + 700
+          )}
+          show={show}
+          isActive={activeLink === AgencyLinks.CLIENTS}
+          options={{ order: 2 }}
+        />
+      </li>
+    </React.Fragment>
+  );
+
+  const workLinks = (
+    <React.Fragment>
+      <li key={0}>
+        <ClickLink
+          link="All"
+          click={clickWithDispath(WorkLinks.ALL, "ALL")}
+          show={show}
+          isActive={activeLink === WorkLinks.ALL}
+          options={{ order: 0 }}
+        />
+      </li>
+      <li key={1}>
+        <ClickLink
+          link="Branding"
+          click={clickWithDispath(WorkLinks.BRANDING, "BRANDING")}
+          show={show}
+          isActive={activeLink === WorkLinks.BRANDING}
+          options={{ order: 1 }}
+        />
+      </li>
+      <li key={2}>
+        <ClickLink
+          link="Digital"
+          click={clickWithDispath(WorkLinks.DIGITAL, "DIGITAL")}
+          show={show}
+          isActive={activeLink === WorkLinks.DIGITAL}
+          options={{ order: 2 }}
         />
       </li>
     </React.Fragment>
@@ -53,8 +153,8 @@ const VerticalNav = ({ links, show, delay = 0 }: NavProps): JSX.Element => {
   return (
     <nav className="vertical-nav">
       <ul className="vertical-nav-links">
-        {links ? (
-          renderLinks()
+        {menu ? (
+          MenuLinks
         ) : (
           <Switch>
             <Route exact path="/">
@@ -63,6 +163,9 @@ const VerticalNav = ({ links, show, delay = 0 }: NavProps): JSX.Element => {
             <Route exact path="/agency">
               {agencyLinks}
             </Route>
+            <Route exact path="/work">
+              {workLinks}
+            </Route>
           </Switch>
         )}
       </ul>
@@ -70,4 +173,8 @@ const VerticalNav = ({ links, show, delay = 0 }: NavProps): JSX.Element => {
   );
 };
 
-export default VerticalNav;
+const mapDispatch = {
+  setFilter: workActions.setVisibilityFilter
+};
+
+export default connect(null, mapDispatch)(VerticalNav);
