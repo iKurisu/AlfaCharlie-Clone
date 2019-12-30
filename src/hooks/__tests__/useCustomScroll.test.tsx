0c1c8b9bfbb597cb1ffa317353740aeb85a8983c
iKurisu <chris.epic01@gmail.com>
2019-12-30 09:41:53 +0100

    add tests

--- src/hooks/__tests__/useCustomScroll.test.tsx
+++ src/hooks/__tests__/useCustomScroll.test.tsx
@@ -1,19 +1,19 @@
 import React, { useRef } from "react";
 import useCustomScroll from "../useCustomScroll";
-import { mount } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 
 describe("useCustomScroll", (): void => {
   const TestComponent = (): JSX.Element => {
     const element = useRef(null);
 
-    const { onWheel } = useCustomScroll(element, {
+    const scroll = useCustomScroll(element, {
       distance: 100,
       duration: 0,
       curve: [0, 0, 0.1, 1]
     });
 
     return (
-      <div className="scroll" onWheel={onWheel}>
+      <div className="scroll" {...scroll}>
         <div
           className="scroll-content"
           ref={element}
@@ -23,15 +23,37 @@ describe("useCustomScroll", (): void => {
     );
   };
 
-  it("updates styles when the user scrolls", (): void => {
-    const wrapper = mount(<TestComponent />);
+  let wrapper: ReactWrapper;
 
-    wrapper.find(".scroll").simulate("wheel", { deltaY: 100 });
-
+  const expectTransformToBe = (value: string): void =>
     expect(
       getComputedStyle(
         wrapper.find(".scroll-content").getDOMNode()
       ).getPropertyValue("transform")
-    ).toBe("translateY(-100px)");
+    ).toBe(value);
+
+  beforeEach((): void => {
+    wrapper = mount(<TestComponent />);
+  });
+
+  it("updates styles when the user scrolls", (): void => {
+    wrapper.find(".scroll").simulate("wheel", { deltaY: 100 });
+
+    expectTransformToBe("translateY(-100px)");
+  });
+
+  it("updates styles when the user touches the screen", (): void => {
+    wrapper
+      .find(".scroll")
+      .simulate("touchStart", { touches: [{ clientY: 200 }] });
+    wrapper
+      .find(".scroll")
+      .simulate("touchMove", { touches: [{ clientY: 100 }] });
+
+    expectTransformToBe("translateY(-100px)");
+
+    wrapper.find(".scroll").simulate("touchEnd");
+
+    expectTransformToBe("translateY(-10100px)");
   });
 });
