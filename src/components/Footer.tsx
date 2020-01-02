88d4d6eb5e30cdc8c92062472c85164863dbd127
iKurisu <chris.epic01@gmail.com>
2020-01-02 09:50:24 +0100

    add footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.scss";
 
-const Footer = (): JSX.Element => <footer className="footer"></footer>;
+const Footer = (): JSX.Element => (
+  <footer className="footer">
+    <div className="footer-content">
+      <div className="footer-contact">
+        <Link to="/contact" />
+        <h3>Let’s work together.</h3>
+        <span>GET IN TOUCH</span>
+      </div>
+      <div className="footer-text"></div>
+    </div>
+  </footer>
+);
 
 export default Footer;
