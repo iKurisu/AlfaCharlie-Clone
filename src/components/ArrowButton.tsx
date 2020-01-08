52df3428be44f88e5cf23a0beea8a10191471309
iKurisu <chris.epic01@gmail.com>
2020-01-08 19:05:52 +0100

    add arrow animation

--- src/components/ArrowButton.tsx
+++ src/components/ArrowButton.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Arrow from "./Arrow";
+import { SubscriberContext } from "../App";
+import { classList } from "utils/class";
+import { isMobile } from "utils/responsive";
 import "./ArrowButton.scss";
 
-const ArrowButton = (): JSX.Element => (
-  <div className="arrow-button">
-    <Arrow />
-  </div>
-);
+const ArrowButton = (): JSX.Element => {
+  const [isVisible, toggle] = useState(false);
+  const subscribe = useContext(SubscriberContext);
+
+  useEffect(() => {
+    if (isMobile()) {
+      subscribe((scroll: number, max: number): void => {
+        toggle(scroll < max * 0.95);
+      });
+    }
+  }, []);
+
+  return (
+    <div className={classList(["arrow-button", { "-show": isVisible }])}>
+      <Arrow />
+    </div>
+  );
+};
 
 export default ArrowButton;
--- src/utils/responsive.ts
+++ src/utils/responsive.ts
@@ -1,1 +1,9 @@
 export const vwToPx = (vw: number): number => (vw * window.innerWidth) / 100;
+
+const isLandscape = (): boolean => window.innerWidth > window.innerHeight;
+
+export const isMobile = (): boolean => {
+  const { innerWidth } = window;
+
+  return innerWidth <= 480 || (isLandscape() && innerWidth <= 823);
+};
