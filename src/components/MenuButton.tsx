24527d53cf4d8ee9043eb3ca875ea4e71a7723dc
iKurisu <chris.epic01@gmail.com>
2020-01-19 09:21:26 +0100

    refactor

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,54 +1,52 @@
-import React from "react";
-import { Dispatch } from "redux";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { AppState } from "store";
 import { menuActions } from "modules/menu";
 import Hamburger from "./menu_button/Hamburger";
 import Cross from "./menu_button/Cross";
+import useDidUpdateEffect from "hooks/useDidUpdateEffect";
 import { classList } from "utils/class";
 import "./MenuButton.scss";
 
 interface MappedState {
   toggled: boolean;
-  onTransition: boolean;
 }
 
 interface MappedActions {
-  toggle: (toggled: boolean) => () => void;
+  toggle: () => void;
 }
 
 type Props = MappedState & MappedActions;
 
-export const MenuButton = ({
-  toggled,
-  onTransition,
-  toggle
-}: Props): JSX.Element => (
-  <div
-    className={classList([
-      "menu-button",
-      { open: toggled, disabled: onTransition }
-    ])}
-    onClick={toggle(toggled)}
-  >
-    <Hamburger />
-    <Cross />
-  </div>
-);
+export const MenuButton = ({ toggled, toggle }: Props): JSX.Element => {
+  const [disabled, disable] = useState(false);
+
+  const disableFor = (ms: number): void => {
+    disable(true);
+    setTimeout(() => disable(false), ms);
+  };
+
+  useDidUpdateEffect(() => {
+    disableFor(2000);
+  }, [toggled]);
+
+  return (
+    <div
+      className={classList(["menu-button", { open: toggled, disabled }])}
+      onClick={toggle}
+    >
+      <Hamburger />
+      <Cross />
+    </div>
+  );
+};
 
 const mapState = ({ menu }: AppState): MappedState => ({
-  toggled: menu.toggled,
-  onTransition: menu.onTransition
+  toggled: menu.toggled
 });
 
-const mapDispatch = (dispatch: Dispatch): MappedActions => ({
-  toggle: toggled => () => {
-    dispatch(menuActions.toggleMenu());
-    setTimeout(
-      () => dispatch(menuActions.endTransition()),
-      toggled ? 1300 : 2300
-    );
-  }
-});
+const mapDispatch: MappedActions = {
+  toggle: menuActions.toggleMenu
+};
 
 export default connect(mapState, mapDispatch)(MenuButton);
