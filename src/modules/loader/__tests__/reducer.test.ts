64828918fe66e4c7fcd3446e5903187a308c4399
iKurisu <chris.epic01@gmail.com>
2020-01-24 11:28:40 +0100

    add footer loader to store

--- src/modules/loader/__tests__/reducer.test.ts
+++ src/modules/loader/__tests__/reducer.test.ts
@@ -1,22 +1,38 @@
 import reducer from "../reducer";
 import { LoaderActionTypes, LoaderState, Types } from "../types";
 
-describe("intro reducer", (): void => {
+describe("loader reducer", (): void => {
   it("returns initial state", (): void => {
     const expectedState: LoaderState = {
-      toggled: false
+      main: false,
+      footer: false
     };
 
     // eslint-disable-next-line @typescript-eslint/no-object-literal-type-assertion
     expect(reducer(undefined, {} as LoaderActionTypes)).toEqual(expectedState);
   });
 
-  it("handles TOGGLE_INTRO", (): void => {
+  it("handles TOGGLE_MAIN_LOADER", (): void => {
     const action: LoaderActionTypes = {
-      type: Types.TOGGLE_LOADER
+      type: Types.TOGGLE_MAIN_LOADER
     };
 
-    expect(reducer(undefined, action)).toEqual({ toggled: true });
-    expect(reducer({ toggled: true }, action)).toEqual({ toggled: false });
+    expect(reducer(undefined, action)).toEqual({ main: true, footer: false });
+    expect(reducer({ main: true, footer: false }, action)).toEqual({
+      main: false,
+      footer: false
+    });
+  });
+
+  it("handles TOGGLE_FOOTER_LOADER", (): void => {
+    const action: LoaderActionTypes = {
+      type: Types.TOGGLE_FOOTER_LOADER
+    };
+
+    expect(reducer(undefined, action)).toEqual({ main: false, footer: true });
+    expect(reducer({ main: false, footer: true }, action)).toEqual({
+      main: false,
+      footer: false
+    });
   });
 });
--- src/modules/loader/reducer.ts
+++ src/modules/loader/reducer.ts
@@ -1,7 +1,8 @@
 import { LoaderActionTypes, LoaderState, Types } from "./types";
 
 const initialState: LoaderState = {
-  toggled: false
+  main: false,
+  footer: false
 };
 
 const reducer = (
@@ -9,8 +10,10 @@ const reducer = (
   action: LoaderActionTypes
 ): LoaderState => {
   switch (action.type) {
-    case Types.TOGGLE_LOADER:
-      return { toggled: !state.toggled };
+    case Types.TOGGLE_MAIN_LOADER:
+      return { ...state, main: !state.main };
+    case Types.TOGGLE_FOOTER_LOADER:
+      return { ...state, footer: !state.footer };
     default:
       return state;
   }
