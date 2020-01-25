937b5a2c130df14b406df79342801d27c7d8aa29
iKurisu <chris.epic01@gmail.com>
2020-01-25 16:33:46 +0100

    add hook to calculate bounding client rect

---
+++ src/hooks/useBoundingClientRect.ts
@@ -0,0 +1,21 @@
+import { RefObject, useState, useEffect } from "react";
+
+/** Provides the bounding client rect of an HTML element. */
+const useBoundingClientRect = (element: RefObject<HTMLElement>): DOMRect => {
+  const [boundingClientRect, setBoundingClientRect] = useState(null);
+
+  const updateBoundingClientRect = (): void => {
+    setBoundingClientRect(element.current.getBoundingClientRect());
+  };
+
+  useEffect((): (() => void) => {
+    updateBoundingClientRect();
+    window.addEventListener("resize", updateBoundingClientRect);
+
+    return () => window.removeEventListener("resize", updateBoundingClientRect);
+  }, []);
+
+  return boundingClientRect;
+};
+
+export default useBoundingClientRect;
