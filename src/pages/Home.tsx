0554da04d6fbcb7fe4978f62d0ea5ff8d65f8440
iKurisu <chris.epic01@gmail.com>
2020-01-28 09:11:32 +0100

    fade in hero when intro is sliding out

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,57 +1,38 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import Hero from "./home/Hero";
 import Info from "./shared/Info";
 import Testimonials from "./shared/Testimonials";
 import FooterArt from "components/FooterArt";
-import { introActions } from "modules/intro";
-import { IntroActionTypes } from "modules/intro/types";
 import "./Home.scss";
 
-interface MappedActions {
-  toggleIntro: () => IntroActionTypes;
-}
+const Home = (): JSX.Element => (
+  <React.Fragment>
+    <div className="row">
+      <Hero />
+    </div>
+    <div className="separator" />
+    <div className="row">
+      <Info
+        header="elegant solutions"
+        image={
+          "https://alfacharlie.b-cdn.net/wp-content/uploads/" +
+          "2019/05/Alfa-Charlie-Creative-Agency-6-1-e1558111141759.jpg"
+        }
+        title="Insight + creativity"
+        text={
+          "We’re adept at understanding what a project needs. Our " +
+          "designs capture the essence of your business, so you can make " +
+          "a more consistent and memorable impact, everywhere."
+        }
+        link="/agency"
+      />
+    </div>
+    <div className="separator -big" />
+    <div className="row">
+      <Testimonials title="brilliant clients" />
+    </div>
+    <FooterArt />
+  </React.Fragment>
+);
 
-type Props = MappedActions;
-
-const Home = ({ toggleIntro }: Props): JSX.Element => {
-  useEffect((): void => {
-    toggleIntro();
-  }, []);
-
-  return (
-    <React.Fragment>
-      <div className="row">
-        <Hero />
-      </div>
-      <div className="separator" />
-      <div className="row">
-        <Info
-          header="elegant solutions"
-          image={
-            "https://alfacharlie.b-cdn.net/wp-content/uploads/" +
-            "2019/05/Alfa-Charlie-Creative-Agency-6-1-e1558111141759.jpg"
-          }
-          title="Insight + creativity"
-          text={
-            "We’re adept at understanding what a project needs. Our " +
-            "designs capture the essence of your business, so you can make " +
-            "a more consistent and memorable impact, everywhere."
-          }
-          link="/agency"
-        />
-      </div>
-      <div className="separator -big" />
-      <div className="row">
-        <Testimonials title="brilliant clients" />
-      </div>
-      <FooterArt />
-    </React.Fragment>
-  );
-};
-
-const mapDispatch: MappedActions = {
-  toggleIntro: introActions.toggleIntro
-};
-
-export default connect(null, mapDispatch)(Home);
+export default Home;
